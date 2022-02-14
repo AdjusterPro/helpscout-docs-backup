c8466c5ddaa84f1d6dd5f789a24115f7edee097b
Ben Dunlap <ben@adjusterpro.com>
2022-02-14 14:21:36 -0600

    get target dir from command line. While at it, specify a source in Gemfile //bdunlap

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 Pulls all collections and their articles using Help Scout's [Docs API](https://developer.helpscout.com/docs-api/) and writes it all out to disk, one file per collection.
 
 Please define HELPSCOUT_DOCS_KEY in env. You can find your API key under *Your Profile -> Authentication -> API Keys*
-
-TODO: Tar & encrypt output and upload to S3.
--- backup-docs.rb
+++ backup-docs.rb
@@ -6,9 +6,11 @@ load 'lib/tools.rb'
 $logger = Logger.new(STDERR)
 $logger.level = Logger::INFO
 
+backup_dir = ARGV[0] || abort("Usage: #{__FILE__} <dir to store backups in>")
+
 get_items('collections')
 .each do |c|
-  filename = "backups/#{c['slug']}.json"
+  filename = "#{backup_dir}/#{c['slug']}.json"
   c['articles'] = get_items("collections/#{c['id']}/articles")
   .tap { |articles| $logger.info("#{c['name']} has #{articles.size} articles") }
   .map { |a| get("articles/#{a['id']}")['article'] }
